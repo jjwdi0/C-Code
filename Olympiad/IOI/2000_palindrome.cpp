96919f63bb4cc6a49ac2aa1954d7be1118928ac9
Jeong Deokin <jjwdi0@naver.com>
2017-04-03 14:39:14 +0900

    Update 2000_palindrome.cpp

--- Olympiad/IOI/2000_palindrome.cpp
+++ Olympiad/IOI/2000_palindrome.cpp
@@ -7,7 +7,7 @@ int max(int X, int Y) { return X>Y?X:Y; }
 int main() {
 	scanf("%d", &N);
 	scanf("%s", A);
-	for(i=0; i<n; i++) B[N-i-1] = A[i];
+	for(int i=0; i<n; i++) B[N-i-1] = A[i];
 	for(int i=1; i<=N; i++) for(int j=1; j<=N; j++) {
 		if(A[i-1] == B[j-1]) lcs[i][j] = lcs[i-1][j-1] + 1;
 		else lcs[i][j] = max(lcs[i-1][j], lcs[i][j-1]);
